e36657653ada21ae1fd51b1c7da72e781caadb21
Sixhei Tartari <sixheitartari12@gmail.com>
2022-01-09 20:59:03 +0100

    updating Layout

--- components/Layout.js
+++ components/Layout.js
@@ -5,12 +5,19 @@ import useStyles from '../utils/styles';
 
 function Layout({ children }) {
   const classes = useStyles();
-
+  /*TODO:
+      1. Add A Link to Homepage
+      2. Add a Cart Link
+      3. Add a Login Link
+      using:
+      NextLink and Link
+  */
   return (
     <div>
       <Head>
         <title>Techocles</title>
       </Head>
+      {/* insert here */}
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Techocles</Typography>
